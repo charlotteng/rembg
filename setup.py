46d03fd180c0ac46b8f1f4e1cd524bf9d3494ca5
charlotteng <69661174+charlotteng@users.noreply.github.com>
2023-04-03 21:17:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     packages=["rembg"],
     python_requires=">3.7, <3.11",
     install_requires=[
-        "Pillow==9.1.0",
+        "pillow“,
         "asyncer>=0.0.2",
         "click>=8.1.3",
         "fastapi>=0.92.0",
